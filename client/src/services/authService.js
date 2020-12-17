adeec400095be996e7aa4c8c419ede1d7e80f339
Mahmut Sarcan <mhmtsrcn58@hotmail.com>
2020-12-17 10:53:58 +0100

    env

--- client/src/services/authService.js
+++ client/src/services/authService.js
@@ -4,7 +4,7 @@ import axios from 'axios'
 class AuthService {
     constructor(){
         let service = axios.create({
-            baseURL: 'http://localhost:5000/',
+            baseURL:process.env.REACT_APP_SERVER_URL,
             withCredentials:true
         })
         this.service = service
