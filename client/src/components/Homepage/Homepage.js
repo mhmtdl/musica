137f3514fbe3abf0d7a2c5c65bb902f2c4af640f
Mahmut Sarcan <mhmtsrcn58@hotmail.com>
2020-12-17 20:28:00 +0100

    styling

--- client/src/components/Homepage/Homepage.js
+++ client/src/components/Homepage/Homepage.js
@@ -30,17 +30,22 @@ export default class Homepage extends Component {
               <div className="inner">
                 <div class="container">
                   <div class="row landing-row d-flex">
-                    <div className="col-lg-6">
+                    <div className="hero">
+
+                    <h1>Where words fail, music speaks.</h1>
+                    <Link to="/login">Try it for free</Link>
                      
-                      <h1 style={{color:'#fafafa',fontFamily:'sans-serif',fontSize:'36px',marginLeft:'10vh'}}>Where words fail, music speaks.</h1>
+                      {/* <h1 style={{color:'#fafafa',fontFamily:'sans-serif',fontSize:'36px',marginLeft:'10vh'}}>Where words fail, music speaks.</h1> */}
                       
                     </div>
                    
-                      <div className='col-lg-6 mr-5 pt-4 '>
-                      <button style={{width:'200px',fontWeight:'bold',height:'45px',fontSize:'larger',borderRadius:'20px',marginLeft:'9vh'}} >
-                      <Link to="/login">Try it for free</Link></button>
+                      {/* <div className='col-lg-6 mr-5 pt-4 '> */}
+
+
+                      {/* <button style={{width:'200px',fontWeight:'bold',height:'45px',fontSize:'larger',borderRadius:'20px',marginLeft:'9vh'}} >
+                      <Link to="/login">Try it for free</Link></button> */}
                            
-                      </div>
+                      {/* </div> */}
                     
                   </div>
                 </div>
